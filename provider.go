9557a0d8f7a262cc97f9ba71784372f516e77c64
Thomas Bishop <tomb@artfulassemblage.com>
2021-04-14 13:45:36 +0100

    GetRecords() now lists all records in given zone

--- provider.go
+++ provider.go
@@ -1,11 +1,13 @@
-// Package libdnstemplate implements a DNS record management client compatible
-// with the libdns interfaces for Mythic Beasts.
-// TODO: This package is a template only. Customize all godocs for actual implementation.
-package libdnstemplate
+package mythicbeasts
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
 
 	"github.com/libdns/libdns"
 )
@@ -16,16 +18,59 @@ import (
 // when methods are called; sync.Once can help with this, and/or you can use a
 // sync.(RW)Mutex in your Provider struct to synchronize implicit provisioning.
 
-// Provider facilitates DNS record manipulation with <TODO: PROVIDER NAME>.
+// Provider facilitates DNS record manipulation with Mythic Beasts.
 type Provider struct {
 	// TODO: put config fields here (with snake_case json
 	// struct tags on exported fields), for example:
-	APIToken string `json:"api_token,omitempty"`
+	KeyID  string `json:"key_id,omitempty"`
+	Secret string `json:"secret,omitempty"`
+	token  mythicAuthResponse
 }
 
-// GetRecords lists all the records in the zone.
+// GetRecords lists all records in given zone.
 func (p *Provider) GetRecords(ctx context.Context, zone string) ([]libdns.Record, error) {
-	return nil, fmt.Errorf("TODO: not implemented")
+	err := p.login()
+	if err != nil {
+		fmt.Errorf("login: provider login failed: %d", err)
+		return nil, err
+	}
+
+	req, err := http.NewRequest("GET", "https://api.mythic-beasts.com/dns/v2/zones/"+zone+"/records", nil)
+	if err != nil {
+		fmt.Errorf("login: provider record request failed: %d", err)
+		return nil, err
+	}
+	req.Header.Set("Authorization", os.ExpandEnv("Bearer "+p.token.Token))
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Errorf("login: provider record request failed: %d", err)
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Errorf("login: failed to read response body: %d", err)
+		return nil, err
+	}
+
+	result := mythicRecordsResponse{}
+	if err := json.Unmarshal(body, &result); err != nil {
+		fmt.Errorf("login: failed to extract JSON data: %d", err)
+		return nil, err
+	}
+	var records []libdns.Record
+
+	for _, r := range result.Records {
+		records = append(records, libdns.Record{
+			Type:  r.Type,
+			Name:  r.Name,
+			Value: r.Value,
+			TTL:   time.Duration(r.TTL) * time.Second,
+		})
+	}
+	return records, nil
 }
 
 // AppendRecords adds records to the zone. It returns the records that were added.
